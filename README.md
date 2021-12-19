660ba97e00c1fc4e9f3a0c07c2f62f9d525ccd83
Hiroshi Maeda <5381052+hir0shim@users.noreply.github.com>
2021-12-19 23:48:35 +0900

    Reproduce the bsc issue

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # truffle-bsc-deploy-investigation
+This is a minimal set of the code to reproduce the issue that Truffle failed to deploy a contract to BSC main net while it worked for test net.
+I basically followed [the official doc](https://docs.binance.org/smart-chain/developer/deploy/truffle.html).
+
+
+# The steps to reproduce
+```
+$ echo ${YOUR MNEMONIC} > .secret
+$ npm install
+$ truffle compile
+$ truffle migrate --network testnet # This should work
+$ truffle migrate --network bsc     # This will fail
+...
+Unhandled error.({ code: -32000, message: 'header not found' })
+...
+```
+
+# Tested Truffle version
+```
+$ truffle version
+Truffle v5.4.25 (core: 5.4.25)
+Solidity v0.5.16 (solc-js)
+Node v14.15.1
+Web3.js v1.5.3
+```
--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -41,11 +41,25 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -81,7 +95,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "^0.6.12", // A version or constraint - Ex. "^0.5.0"
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -104,13 +118,13 @@ module.exports = {
   // $ truffle migrate --reset --compile-all
   //
   // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
+  // enabled: false,
+  // host: "127.0.0.1",
+  // adapter: {
+  //   name: "sqlite",
+  //   settings: {
+  //     directory: ".db"
+  //   }
+  // }
   // }
 };
